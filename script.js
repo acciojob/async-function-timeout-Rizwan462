aa895aa51b24d936c2461ac7a9e2626944646009
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-09-01 21:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', async function(e){
+	e.preventDefault();
+	const text = document.getElementById('text').value;
+	const delay = parseInt(document.getElementById('delay').value);
+	const output = document.getElementById('output');
+
+	if(!text || isNaN(delay) || delay < 0) {
+		output.textContent = 'Please provide valid input values.'
+		return;	
+	}
+	output.textContent = 'Waiting...';
+
+	await delayMessage(delay);
+	output.textContent = text;
+});
+function delayMessage(seconds) {
+	return new Promise(res =>{
+		setTimeout(res, seconds * 1000);
+	});
+	
+}
+
+
+
+
